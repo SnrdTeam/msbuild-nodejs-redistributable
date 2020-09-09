9825f8a754cdd4d6c41480b18a12cea71608c697
XGuppy <xguppy@yandex.ru>
2020-09-09 15:09:01 +0400

    Node process exit after pipe master's is dead

--- src/UnitTesting/reporters/MachineReadablePipeReporter.js
+++ src/UnitTesting/reporters/MachineReadablePipeReporter.js
@@ -4,14 +4,14 @@ const process = require('process');
 const PIPE_NAME = 'ReporterJasminePipe' + process.ppid;
 const PIPE_PATH = (os.platform == 'win32' ? '\\\\.\\pipe\\' : '/var/pipes/') + PIPE_NAME;
 
-let stream = net.connect(PIPE_PATH);
+let stream = net.connect(PIPE_PATH).addListener("close", () => process.exit(0));
 
 const MachineReadablePipeReporter = {
     specDone: function (result) {
         //Insert dot between construction's name
         //Delete trailing spaces and replace remain spaces with underscores
-        let suits = result.fullName.replace(result.description, ' ').trim().replace(/ /g, '_');
-        let spec = result.description.trim().replace(/ /g, '_');
+        let suits = result.fullName.replace(result.description, ' ').trim().replace(/ /g, '_').replace(/\n/g, '');
+        let spec = result.description.trim().replace(/ /g, '_').replace(/\n/g, '');
         stream.write(suits + '.' + spec + os.EOL);
         stream.write(result.status + os.EOL);
     },
